15a00c788125fe33dc75e30a73c2134ea2570261
karthik kjs <104209441+karthikgeek1@users.noreply.github.com>
2023-06-04 09:36:37 +0530

    apitest

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const app = express();
+const otpgenerator = require('otp-generator')
+const apidata = require('./data.json')
+
+app.get('/services', (req,res)=>{
+    res.send(apidata)
+})
+
+app.get('/',(req,res)=>{
+    res.send('hello')
+})
+
+app.listen(3000, ()=>{
+    console.log('listening')
+})
